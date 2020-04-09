b2020000f477c127f7f371ecc40efeb8859dab46
kokdot <kok.74@mail.ru>
2020-04-09 20:09:16 +0000

    lesson_7

---
+++ cargo_van.rb
@@ -0,0 +1,25 @@
+require_relative('van')
+
+class CargoVan < Van
+  def initialize(ability)
+    @ability = ability.to_i
+    @volume = 0
+  end
+
+  def free_volume
+    @ability - @volume
+  end
+
+  def populated_volume
+    @volume
+  end
+
+  def take_volume(volume)
+    if @ability - @volume >= volume
+      @volume += volume 
+    else
+      puts "Места в вагоне не достаточно."
+    end
+  end
+
+end
---
+++ passenger_van.rb
@@ -0,0 +1,24 @@
+require_relative('van')
+
+class PassengerVan < Van
+def initialize(capacity)
+  @capacity = capacity
+  @busy = 0
+end
+
+def free_places
+  @capacity - @busy
+end
+
+def populated_places
+  @busy
+end
+
+def take_place
+  if @capacity > @busy
+    @busy += 1 
+  else
+    puts "Мест нет"
+  end
+end
+end
